cb0bcd154cd42b46435a970686245ea7d3f6c453
uddinsala <salauddin.shams@gmail.com>
2022-12-27 04:32:54 +0600

    update js

--- src/helpers/theme.js
+++ src/helpers/theme.js
@@ -55,9 +55,25 @@ const theme = createTheme({
       width: 300,
       fontSize: 16,
     },
-    appBar: {
-      fontSize: 16,
+  appBar: {
+    position: 'absolute',
+    marginLeft: drawerWidth,
+    [theme.breakpoints.up('md')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
     },
+  },
+  navIconHide: {
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+    [theme.breakpoints.up('md')]: {
+      position: 'relative',
+    },
+  },
   },
   page: {
     padding: 16,
